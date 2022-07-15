483c82dcda687fd3b5deb252de40775c5fae9dda
tyron40 <107443273+tyron40@users.noreply.github.com>
2022-07-15 09:46:18 -0400

    image changes

--- src/components/Covid19WaiverContainer.vue
+++ src/components/Covid19WaiverContainer.vue
@@ -4,8 +4,8 @@
       <h2>COVID-19 Policy</h2>
     <h3>Masks</h3>
       <ul>
-        <li>Requirement for masks will be dictated by CDC (United States Centers for Disease Control and Prevention) recommendations, FIRST (For Inspiration and Recognition in Science and Technology) policy, local policy, and museum policy, deferring to whichever requirement is most strict</li>
-      <li>In cases when masks are required</li>
+        <li>Requirement for masks will be dictated by CDC (United States Centers for Disease Control and Prevention) recommendations, FIRST (For Inspiration and Recognition in Science and Technology) policy, local policy, and museum policy,<strong>deferring to whichever requirement is most strict</strong></li>
+      <li><strong>In cases when masks are required</strong></li>
       <ul>
         <li>Masks are required to be worn by all people (ages over 3) at all times</li>
           <li>N95/KN95/KF94 masks are recommended</li>
@@ -13,7 +13,7 @@
           <li>Masks should be tightly woven and well-fitting — they must fit—and stay—over your nose and mouth, with no gaps on the sides, top or bottom</li>
           <li>Masks must not have exhalation valves or vents</li>
           <li>Staff and mentors may require you to replace your mask with another at their discretion</li>
-          <li>Things that do NOT qualify as adequate protection:</li>
+          <li>Things that do <strong>NOT</strong> qualify as adequate protection:</li>
           <ul>
           <li>Face shields alone (can be worn for added protection above and beyond mask)</li>
           <li>Bandanas/scarves</li>
@@ -22,29 +22,44 @@
           <li>Masks with visible holes, including mesh or lace</li>
           </ul>
           <li>If wearing a cloth mask, consider double masking</li>
-          <img :src="image">
-          <li>Even in cases when masks are not required, disposable masks will be available on request</li>
+          <figure>
+            <img src="./../assets/better-fitting-mask-info.png" 
+                  id="masking-instructions__recommendations-image" 
+                  alt="An infographic describing ways to have a better fit and extra protection with cloth and disposable masks" 
+                  aria-describedby="masking-instructions__recommendations-image-description">
+                  <figcaption class="sr-only" id="masking-instructions__recommendations-image-description">
+                    An infographic that says 'ways to have a better fit and extra protection with cloth and disposable masks: wear two masks (disposable mask worn underneath and cloth mask on top), combine either a cloth mask or disposable mask with a fitter or brace, knot and tuck ear loops of a 3-ply mask where they join the edge of the mask - for disposable procedure masks, fold and tuck the unneeded material under the edges (for instructions see https://youtu.be/GzTAZDsNBe0), use masks that attach behind the neck and head with either elastic bands or ties (instead of ear loops)'. The infographic also shows good examples of mask wearing (disposable mask under cloth mask, and alternatively a cloth mask with a fitter). Finally, the infographic shows knotting the ear loops of masks for a better fit, folding and tucking in unneeded material for a better fit, and then checking for a close fit
+                  </figcaption>
+          </figure>
+
+          <li>Even in cases when masks are not required, <strong><mark>disposable masks will be available on request</mark></strong></li>
       </ul>
      <h3>Vaccines</h3>
       <ul>
-          <li>Everyone who is eligible should be fully vaccinated including booster dose at the time they are eligible for booster in order to participate in person</li>
-          <img :src="boosterImage">
+          <li>Everyone who is eligible should be fully vaccinated <strong>including booster</strong> dose at the time they are eligible for booster in order to participate in person</li>
+          <figure>
+            <img src="./../assets/different-vaccination-types.png" 
+                  id="vaccines__types-image" 
+                  alt="An infographic describing Covid-19 booster shot recommendations"
+                  aria-describedby="vaccines__types-image-description">
+                  <figcaption class="sr-only" id="vaccines__types-image-description">Moderna 5 months after second dose; Pfizer 5 months after second dose; Johnson & Johnson 2 months after single dose. Get boosted</figcaption>
+          </figure>
           <li>ALL attendees/participants are required to be fully vaccinated and, if eligible, boosted 2 weeks or longer before attending a meeting/event in person</li>
           <li>If you have been in contact with someone who has tested positive to COVID-19 (regardless of your vaccination status), prior to attending your next meeting/event you must</li>
           <ul>
-          <li>Have 2 negative OTC tests OR</li>
-          <li>Have 1 negative PCR test OR</li>
+          <li>Have 2 negative OTC tests <strong>OR</strong></li>
+          <li>Have 1 negative PCR test <strong>OR</strong></li>
           <li>Not attend in person for at least 10 days from contact</li>
           </ul>
-          <li>If you are unable to be vaccinated, you must have a negative test from within 3 days prior to attending an in-person meeting/event</li>
+          <li><strong><mark>If you are unable to be vaccinated,</mark></strong> you must have a negative test from within 3 days prior to attending an in-person meeting/event</li>
       </ul>
       <h3>Food/drink</h3>
       <ul>
         <li>Recommend eating/drinking outside or in the FoodWorks area</li>
-        <li>In cases when masks are required</li>
+        <li><strong>In cases when masks are required</strong></li>
         <ul>
-        <li>Can eat/drink outside, must be 6 ft apar</li>
-        <li>Can eat/drink inside in FoodWorks area, 6 ft apart, in a group of no more than 6 robotics folks</li>
+        <li>Can eat/drink <strong>outside</strong>, must be 6 ft apar</li>
+        <li>Can eat/drink inside in <strong>FoodWorks</strong> area, 6 ft apart, in a group of no more than 6 robotics folks</li>
         </ul>
       <h3>Contact tracing</h3>
       <ul>
@@ -70,8 +85,8 @@
       <p>*This policy may be updated at the discretion of staff/mentors at any time, and will be posted if/when it is updated</p>
       <p>** If support with acquiring COVID-19 tests is needed, please contact a staff member/mentor for assistance</p>
       <section class="form-inputs-container">
-        <section>
-          <p>policy text</p>
+       
+          
           <div class="waiver__input-container">
             <input type="checkbox" v-model="agreeToCovid19Waiver"
                     id="agree-to-covid19-waiver"
@@ -92,9 +107,9 @@
           </div>
              
         </section>
-      
-      </section>
+
     </form>
+    
 </template>
 
 <script>
@@ -106,10 +121,9 @@ export default {
   },
   data(){
     return {
+      isHidden: true,
       agreeToCovid19Waiver: false,
       vaccinationStatus: '',
-      image: 'https://www.bing.com/images/blob?bcid=Tikujby6T2cE1w',
-      boosterImage:'https://www.bing.com/images/blob?bcid=TjXU2.ge9WgEOw'
     }
   }
 }
@@ -141,5 +155,14 @@ div.waiver__input-container {
 }
 ul {
 text-align: left;
+}
+.sr-only:not(:focus):not(:active) {
+  clip: rect(0 0 0 0); 
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap; 
+  width: 1px;
 }
 </style>
