927e065570048277c909ecc7edba59a09c60a74f
tyron40 <107443273+tyron40@users.noreply.github.com>
2022-08-02 16:55:18 -0400

    fix covid19 size adjustment

--- src/components/Covid19WaiverContainer.vue
+++ src/components/Covid19WaiverContainer.vue
@@ -1,60 +1,60 @@
 <template>
-    <form @submit="handleSubmitEvent">
-      <!-- Form Example (submitting currently does nothing with the data) -->
-      <h2>COVID-19 Policy</h2>
-      <fieldset style="border: none;"  id="covidpolicy">
-      <h3 style="color:blue">Masks</h3>
+  <form @submit="handleSubmitEvent">
+    <!-- Form Example (submitting currently does nothing with the data) -->
+    <h2>COVID-19 Policy</h2>
+    <section class="form-inputs-container">
+    <h3>Masks</h3>
+    <ul>
+      <li>Requirement for masks will be dictated by CDC (United States Centers for Disease Control and Prevention) recommendations, FIRST (For Inspiration and Recognition in Science and Technology) policy, local policy, and museum policy, deferring to whichever requirement is most strict</li>
+      <li>In cases when masks are required</li>
       <ul>
-        <li>Requirement for masks will be dictated by CDC (United States Centers for Disease Control and Prevention) recommendations, FIRST (For Inspiration and Recognition in Science and Technology) policy, local policy, and museum policy, deferring to whichever requirement is most strict</li>
-        <li>In cases when masks are required</li>
-        <ul>
-          <li>Masks are required to be worn by all people (ages over 3) at all times</li>
-          <li>N95/KN95/KF94 masks are recommended</li>
+        <li>Masks are required to be worn by all people (ages over 3) at all times</li>
+        <li>N95/KN95/KF94 masks are recommended</li>
+      </ul>
+      <li>Masks should be tightly woven and well-fitting — they must fit—and stay—over your nose and mouth, with no gaps on the sides, top or bottom</li>
+      <li>Masks must not have exhalation valves or vents</li>
+      <li>Staff and mentors may require you to replace your mask with another at their discretion</li>
+      <li>Things that do NOT qualify as adequate protection:</li>
+      <ul>
+        <li>Face shields alone (can be worn for added protection above and beyond mask)</li>
+        <li>Bandanas/scarves</li>
+        <li>Ski masks/balaclavas</li>
+        <li>Neck gaiters</li>
+        <li>Masks with visible holes, including mesh or lace</li>
         </ul>
-        <li>Masks should be tightly woven and well-fitting — they must fit—and stay—over your nose and mouth, with no gaps on the sides, top or bottom</li>
-        <li>Masks must not have exhalation valves or vents</li>
-        <li>Staff and mentors may require you to replace your mask with another at their discretion</li>
-        <li>Things that do NOT qualify as adequate protection:</li>
-        <ul>
-          <li>Face shields alone (can be worn for added protection above and beyond mask)</li>
-          <li>Bandanas/scarves</li>
-          <li>Ski masks/balaclavas</li>
-          <li>Neck gaiters</li>
-          <li>Masks with visible holes, including mesh or lace</li>
-          </ul>
-          <li>If wearing a cloth mask, consider double masking</li>
-          <figure>
-            <img src="./../assets/better-fitting-mask-info.png" 
-                  id="masking-instructions__recommendations-image" 
-                  alt="An infographic describing ways to have a better fit and extra protection with cloth and disposable masks" 
-                  aria-describedby="masking-instructions__recommendations-image-description"
-                  class="image-fit">
-                  <figcaption class="sr-only" id="masking-instructions__recommendations-image-description">
-                    An infographic that says 'ways to have a better fit and extra protection with cloth and disposable masks: wear two masks (disposable mask worn underneath and cloth mask on top), combine either a cloth mask or disposable mask with a fitter or brace, knot and tuck ear loops of a 3-ply mask where they join the edge of the mask - for disposable procedure masks, fold and tuck the unneeded material under the edges (for instructions see https://youtu.be/GzTAZDsNBe0), use masks that attach behind the neck and head with either elastic bands or ties (instead of ear loops)'. The infographic also shows good examples of mask wearing (disposable mask under cloth mask, and alternatively a cloth mask with a fitter). Finally, the infographic shows knotting the ear loops of masks for a better fit, folding and tucking in unneeded material for a better fit, and then checking for a close fit
-                  </figcaption>
-          </figure>
+        <li>If wearing a cloth mask, consider double masking</li>
+        <figure>
+          <img src="./../assets/better-fitting-mask-info.png" 
+                id="masking-instructions__recommendations-image" 
+                alt="An infographic describing ways to have a better fit and extra protection with cloth and disposable masks" 
+                aria-describedby="masking-instructions__recommendations-image-description"
+                class="image-fit">
+                <figcaption class="sr-only" id="masking-instructions__recommendations-image-description">
+                  An infographic that says 'ways to have a better fit and extra protection with cloth and disposable masks: wear two masks (disposable mask worn underneath and cloth mask on top), combine either a cloth mask or disposable mask with a fitter or brace, knot and tuck ear loops of a 3-ply mask where they join the edge of the mask - for disposable procedure masks, fold and tuck the unneeded material under the edges (for instructions see https://youtu.be/GzTAZDsNBe0), use masks that attach behind the neck and head with either elastic bands or ties (instead of ear loops)'. The infographic also shows good examples of mask wearing (disposable mask under cloth mask, and alternatively a cloth mask with a fitter). Finally, the infographic shows knotting the ear loops of masks for a better fit, folding and tucking in unneeded material for a better fit, and then checking for a close fit
+                </figcaption>
+        </figure>
 
-          <li>Even in cases when masks are not required, <strong><mark>disposable masks will be available on request</mark></strong></li>
+        <li>Even in cases when masks are not required, <strong><mark>disposable masks will be available on request</mark></strong></li>
       </ul>
       <h3>Vaccines</h3>
       <ul>
-          <li>Everyone who is eligible should be fully vaccinated <strong>including booster</strong> dose at the time they are eligible for booster in order to participate in person</li>
-          <figure>
-            <img src="./../assets/different-vaccination-types.png" 
-                  id="vaccines__types-image" 
-                  alt="An infographic describing Covid-19 booster shot recommendations"
-                  aria-describedby="vaccines__types-image-description"
-                  class="image-fit">
-                  <figcaption class="sr-only" id="vaccines__types-image-description">Moderna 5 months after second dose; Pfizer 5 months after second dose; Johnson & Johnson 2 months after single dose. Get boosted</figcaption>
-          </figure>
-          <li>ALL attendees/participants are required to be fully vaccinated and, if eligible, boosted 2 weeks or longer before attending a meeting/event in person</li>
-          <li>If you have been in contact with someone who has tested positive to COVID-19 (regardless of your vaccination status), prior to attending your next meeting/event you must</li>
-          <ul>
+        <li>Everyone who is eligible should be fully vaccinated <strong>including booster</strong> dose at the time they are eligible for booster in order to participate in person</li>
+        <figure>
+          <img src="./../assets/different-vaccination-types.png" 
+                id="vaccines__types-image" 
+                alt="An infographic describing Covid-19 booster shot recommendations"
+                aria-describedby="vaccines__types-image-description"
+                class="image-fit">
+                <figcaption class="sr-only" id="vaccines__types-image-description">Moderna 5 months after second dose; Pfizer 5 months after second dose; Johnson & Johnson 2 months after single dose. Get boosted</figcaption>
+        </figure>
+        <li>ALL attendees/participants are required to be fully vaccinated and, if eligible, boosted 2 weeks or longer before attending a meeting/event in person</li>
+        <li>If you have been in contact with someone who has tested positive to COVID-19 (regardless of your vaccination status), prior to attending your next meeting/event you must</li>
+        <ul>
           <li>Have 2 negative OTC tests <strong>OR</strong></li>
           <li>Have 1 negative PCR test <strong>OR</strong></li>
           <li>Not attend in person for at least 10 days from contact</li>
-          </ul>
-          <li><strong><mark>If you are unable to be vaccinated,</mark></strong> you must have a negative test from within 3 days prior to attending an in-person meeting/event</li>
+        </ul>
+        <li><strong><mark>If you are unable to be vaccinated,</mark></strong> you must have a negative test from within 3 days prior to attending an in-person meeting/event</li>
       </ul>
       <h3>Food/drink</h3>
       <ul>
@@ -71,7 +71,7 @@
       <ul>
         <li>Must sign in to meeting/event</li>
         <li>Must be officially signed up with contact info in FIRST Dashboard and/or have contact info registered with the team</li>
-        <li>If you’ve attended a robotics meeting/event and had a positive COVID-19 test following that meeting/event, notify engineering@kaleideum.org so that contacts can be informed that they have had a positive contact (names of those with a positive test will not be shared by mentors/staff unless someone requests their own name be shared; only notification that someone was in contact with a person who had a positive COVID-19 test result)</li>
+        <li>If you've attended a robotics meeting/event and had a positive COVID-19 test following that meeting/event, notify engineering@kaleideum.org so that contacts can be informed that they have had a positive contact (names of those with a positive test will not be shared by mentors/staff unless someone requests their own name be shared; only notification that someone was in contact with a person who had a positive COVID-19 test result)</li>
       </ul>
       <h3>Additional Information</h3>
       <ul>
@@ -89,40 +89,37 @@
       </ul>
       <p>*This policy may be updated at the discretion of staff/mentors at any time, and will be posted if/when it is updated</p>
       <p>** If support with acquiring COVID-19 tests is needed, please contact a staff member/mentor for assistance</p>
-      <section class="form-inputs-container">
-       
-          
-          <div class="waiver__input-container">
-            <input type="checkbox" v-model="agreeToCovid19Waiver"
-            required
-                    id="agree-to-covid19-waiver"
-                    name="agree-to-covid19-waiver"/>
-            <label for="agree-to-covid19-waiver"><strong> agree to abide by this full policy (or I agree as a parent/guardian on behalf of a child participant)</strong></label>
-          </div>
-          <div class="waiver__input-container">
-            <input type="radio" 
-            v-model="vaccinationStatus"   
-            required
-            value="vaccinated"
-              id="attest-to-fully-vaccinated"
-              name="vaccination-status"/>
-            <label for="attest-to-fully-vaccinated"><strong> have been fully vaccinated, and boosted if eligible (or I attest as a parent/guardian that my child participant has been fully vaccinated and boosted)</strong></label>
-          </div>
-          <div class="waiver__input-container">
-            <input type="radio"
-             v-model="vaccinationStatus" 
-             required
-             value="unvaccinated"
-              id="attest-to-testing-requirements"
-              name="vaccination-status"/>
-            <label for="attest-to-testing-requirements"><strong> have not been both fully vaccinated, and boosted if eligible, at this time, and agree to abide by testing requirements (or I attest as a parent/guardian that my child participant has not been both fully vaccinated and boosted at this time, and agree to abide by testing requirements)</strong></label>
-          </div>    
-        </section>
-        </fieldset>
+      <div class="waiver__input-container">
+        <input type="checkbox" v-model="agreeToCovid19Waiver"
+        required
+                id="agree-to-covid19-waiver"
+                name="agree-to-covid19-waiver"/>
+            <label for="agree-to-covid19-waiver"><strong>I agree to abide by this full policy (or I agree as a parent/guardian on behalf of a child participant)</strong></label>
+      </div>
+      <div class="waiver__input-container">
+        <input type="radio"
+                v-model="vaccinationStatus"
+                required
+                value="vaccinated"
+                id="attest-to-fully-vaccinated"
+                name="vaccination-status"/>
+            <label for="attest-to-fully-vaccinated"><strong>I have been fully vaccinated, and boosted if eligible (or I attest as a parent/guardian that my child participant has been fully vaccinated and boosted)</strong></label>
+      </div>
+      <div class="waiver__input-container">
+        <input type="radio"
+                v-model="vaccinationStatus"
+                required
+                value="unvaccinated"
+                id="attest-to-testing-requirements"
+                name="vaccination-status"/>
+            <label for="attest-to-testing-requirements"><strong>I have not been both fully vaccinated, and boosted if eligible, at this time, and agree to abide by testing requirements (or I attest as a parent/guardian that my child participant has not been both fully vaccinated and boosted at this time, and agree to abide by testing requirements)</strong></label>
+      </div>    
+      <button :disabled="isLoading" type="submit" class="primary-action">
+        {{ isLoading ? 'Please Wait...'  : 'Agree and Submit' }}
+      </button>
+    </section>
+  </form>
 
-      <button :disabled="isLoading" type="submit" class="primary-action">{{isLoading ? 'Please Wait...' : 'Agree and Submit'}}</button>
-    </form>
-    
 </template>
 
 <script>
@@ -141,7 +138,7 @@ export default {
   methods: {
     handleSubmitEvent(e) {
       e.preventDefault();
-      let data={
+      let data = {
         agreeToCovid19Waiver: this.agreeToCovid19Waiver,
         vaccinationStatus: this.vaccinationStatus,
       };
@@ -149,13 +146,18 @@ export default {
     }
   }
 }
-
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h2 {
   margin: 2em 0 0;
+}
+h3 {
+  text-align: left;
+}
+p {
+  text-align: left;
+  font-weight: bold;
 }
 form {
   display: flex;
@@ -166,7 +168,7 @@ form {
   max-width: 50em;
 }
 div.waiver__input-container {
-  margin: 1em;
+  margin-bottom: 0.5em;
   display: flex;
   align-items: flex-start;
   text-align: left;
@@ -176,7 +178,7 @@ div.waiver__input-container {
   justify-content: space-between;
 }
 ul {
-text-align: left;
+  text-align: left;
 }
 .sr-only:not(:focus):not(:active) {
   clip: rect(0 0 0 0); 
@@ -188,8 +190,8 @@ text-align: left;
   width: 1px;
 }
 .image-fit {
-height: 60%;
-width:60%;
-object-fit: cover;
+  max-width: 56vw;
+  object-fit: scale-down;
+  margin-left: -2.5em;
 }
 </style>
