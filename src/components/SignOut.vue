5f1ca5e0a9560a98e3bf7bf4862d0cab52791d67
tyron40 <107443273+tyron40@users.noreply.github.com>
2022-08-03 17:24:14 -0400

    sign in/out fields

--- src/components/SignOut.vue
+++ src/components/SignOut.vue
@@ -1,5 +1,7 @@
 <template>
-  <form>
+  <ThankYou v-if="submittedSuccessfully"/>
+  <SomethingWentWrong v-if="hasSubmissionError"/>
+  <form v-if="!submittedSuccessfully && !hasSubmissionError" @submit.prevent="handleGoToThankYou">
         <!-- Form Example (submitting currently does nothing with the data) -->
         <h2>Please Sign out</h2>
         <section class="form-inputs-container">
@@ -14,16 +16,45 @@
                     v-model="signOutName" />
           </div>
         </section>
-        <button class="primary-action">Submit</button>
+        <button class="primary-action" :disabled="isLoading">{{isLoading ? 'Please Wait...' : 'Submit'}}</button>
       </form>  
 </template>
 
 <script>
+import ThankYou from './ThankYou.vue'
+import SomethingWentWrong from './SomethingWentWrong.vue';
+import callSheetAPI from '@/utils/api';
 export default {
-  name: 'SignOut',
-  data(){
-    return {
-      signOutName: ''
+    name: "SignOut",
+    data() {
+        return {
+            signOutName: '',
+            submittedSuccessfully: false,
+            hasSubmissionError: false,
+            isLoading: false,
+            formGoogleSheetName: 'visits',
+        };
+    },
+    components: { ThankYou, SomethingWentWrong },
+    methods: {
+    handleGoToThankYou() {
+      this.isLoading = true;
+      let dataToSend = {
+        participantName: this.signOutName,
+        action: 'Sign Out',
+        formGoogleSheetName: this.formGoogleSheetName,
+      };
+      let xhr = callSheetAPI(dataToSend);
+      let self = this;
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+          //success
+          self.submittedSuccessfully = true;
+        }else if(xhr.status !== 200){
+          //failure
+          self.hasSubmissionError = true;
+        }
+      };
     }
   }
 }
